00372d5962aded21a2f22d6b047ea240ed757d81
Fabian Keck <fabian.keck@adition.com>
2024-04-30 12:12:57 +0200

    Add insert mode escape sequence to init.lua

--- init.lua
+++ init.lua
@@ -180,6 +180,8 @@ vim.keymap.set('n', '<left>', '<cmd>echo "Use h to move!!"<CR>')
 vim.keymap.set('n', '<right>', '<cmd>echo "Use l to move!!"<CR>')
 vim.keymap.set('n', '<up>', '<cmd>echo "Use k to move!!"<CR>')
 vim.keymap.set('n', '<down>', '<cmd>echo "Use j to move!!"<CR>')
+-- Binds Esc to jk
+vim.keymap.set('i', 'jk', '<Esc>')
 
 -- Keybinds to make split navigation easier.
 --  Use CTRL+<hjkl> to switch between windows
