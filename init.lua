d96cd8870235a93f2697cd54061cca558c52eed4
Fabian Keck <fabian.keck@adition.com>
2024-06-04 08:28:10 +0200

    Add keymap for entering netrw to init.lua

--- init.lua
+++ init.lua
@@ -198,6 +198,8 @@ vim.keymap.set('n', '<Leader>ws', '<C-w>s', { desc = 'Split window horizontally'
 vim.keymap.set('n', '<Leader>wv', '<C-w>s', { desc = 'Split window vertically' })
 vim.keymap.set('n', '<Leader>wd', '<C-w><C-q>', { desc = 'Close current window' })
 
+vim.keymap.set('n', '<Leader>.', ':Explore<cr>', { desc = 'Explore current directory' })
+
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
 
