da8fdb5ffa599721794a312ab380323c008245fd
Fabian Keck <fabian.keck@adition.com>
2024-12-05 14:19:11 +0100

    add org-raom plugin

--- init.lua
+++ init.lua
@@ -277,6 +277,27 @@ require('lazy').setup({
       -- })
     end,
   },
+  {
+    'chipsenkbeil/org-roam.nvim',
+    tag = '0.1.1',
+    dependencies = {
+      {
+        'nvim-orgmode/orgmode',
+        tag = '0.3.7',
+      },
+    },
+    config = function()
+      require('org-roam').setup {
+        directory = '~/one-drive/org-roam/',
+        -- optional
+        org_files = {
+          '~/another_org_dir',
+          '~/some/folder/*.org',
+          '~/a/single/org_file.org',
+        },
+      }
+    end,
+  },
   -- loads ocaml plugin
   { 'tjdevries/ocaml.nvim' },
   -- Here is a more advanced example where we pass configuration
