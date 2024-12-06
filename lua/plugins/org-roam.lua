50694c273b6d9d0fde8778d15108ea29e6d53bfd
Fabian Keck <fabian.keck@posteo.de>
2024-12-06 20:31:10 +0100

    Configure org-roam

--- lua/plugins/org-roam.lua
+++ lua/plugins/org-roam.lua
@@ -16,6 +16,31 @@ return {
         '~/some/folder/*.org',
         '~/a/single/org_file.org',
       },
+      -- extensions.dailies.Templates
+      extensions = {
+        dailies = {
+          templates = {
+            d = {
+              description = 'daily',
+              template = '* absences\n** planned\n- \n** actual\n- \n* daily topics\n** %?',
+              target = '%<%Y-%m-%d>.org',
+            },
+          },
+          bindings = {
+            ---Capture today's note.
+            capture_today = '<prefix>dT',
+
+            ---Capture tomorrow's note.
+            capture_tomorrow = '<prefix>dM',
+
+            ---Navigate to today's note.
+            goto_today = '<prefix>dt',
+
+            ---Navigate to tomorrow's note.
+            goto_tomorrow = '<prefix>dm',
+          },
+        },
+      },
     }
   end,
 }
