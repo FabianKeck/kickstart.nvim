7b08ba223a79cfa4f42505a982132c334a4f4dcf
Fabian Keck <fabian.keck@adition.com>
2025-01-06 12:36:23 +0100

    Update which key config to match new structure

--- lua/plugins/which-key.lua
+++ lua/plugins/which-key.lua
@@ -16,25 +16,34 @@
 return { -- Useful plugin to show you pending keybinds.
   'folke/which-key.nvim',
   event = 'VeryLazy', -- Sets the loading event to 'VimEnter'
-  config = function() -- This is the function that runs, AFTER loading
-    require('which-key').setup()
-
-    -- Document existing key chains
-    require('which-key').register {
-      ['<leader>c'] = { name = '[C]ode', _ = 'which_key_ignore' },
-      ['<leader>d'] = { name = '[D]ocument', _ = 'which_key_ignore' },
-      ['<leader>r'] = { name = '[R]ename', _ = 'which_key_ignore' },
-      ['<leader>s'] = { name = '[S]earch', _ = 'which_key_ignore' },
-      --['<leader>w'] = { name = '[W]orkspace', _ = 'which_key_ignore' },
-      ['<leader>w'] = { name = '[W]indow', _ = 'which_key_ignore' },
-      ['<leader>t'] = { name = '[T]oggle', _ = 'which_key_ignore' },
-      ['<leader>h'] = { name = 'Git [H]unk', _ = 'which_key_ignore' },
-      ['<leader>n'] = { name = 'Org Roam [N]ode', _ = 'which_key_ignore' },
-      ['<leader>nd'] = { name = 'Org Roam [D]aily', _ = 'which_key_ignore' },
-    }
-    -- visual mode
-    require('which-key').register({
-      ['<leader>h'] = { 'Git [H]unk' },
-    }, { mode = 'v' })
-  end,
+  opts = {
+    spec = {
+      { '<leader>c', group = '[C]ode' },
+      { '<leader>c_', hidden = true },
+      { '<leader>d', group = '[D]ocument' },
+      { '<leader>d_', hidden = true },
+      { '<leader>h', group = 'Git [H]unk' },
+      { '<leader>h_', hidden = true },
+      { '<leader>o', group = '[O]rg mode' },
+      { '<leader>o_', hidden = true },
+      { '<leader>n', group = 'Org Roam [N]ode' },
+      { '<leader>n_', hidden = true },
+      { '<leader>na', group = 'Org Roam [A]lias' },
+      { '<leader>na_', hidden = true },
+      { '<leader>nd', group = 'Org Roam [D]aily' },
+      { '<leader>nd_', hidden = true },
+      { '<leader>r', group = '[R]ename' },
+      { '<leader>r_', hidden = true },
+      { '<leader>s', group = '[S]earch' },
+      { '<leader>s_', hidden = true },
+      { '<leader>t', group = '[T]oggle' },
+      { '<leader>t_', hidden = true },
+      { '<leader>w', group = '[W]indow' },
+      { '<leader>w_', hidden = true },
+      { '<leader>b', group = '[B]uffer' },
+      { '<leader>b_', hidden = true },
+      -- visual mode
+      { '<leader>h', desc = 'Git [H]unk', mode = 'v' },
+    },
+  },
 }
