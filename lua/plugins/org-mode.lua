e40f75e3745d06de826fcc3450f543487ae6cd10
Fabian Keck <fabian.keck@adition.com>
2025-11-17 17:44:44 +0100

    Fix org-mode Option+Enter keybinding
    - Add Option+Enter (M-CR) mapping to insert new headings in org-mode
    - Use insert_heading_respect_content for context-aware heading insertion
    - Works from anywhere in the file (not just on existing headlines)
    - Properly exits insert mode, runs the command, and re-enters insert mode
    - Requires iTerm2 to have Left Option key set to 'Esc+' in Preferences

--- lua/plugins/org-mode.lua
+++ lua/plugins/org-mode.lua
@@ -7,19 +7,25 @@ return {
     require('orgmode').setup {
       org_agenda_files = { os.getenv 'ROAM_DIR' .. '/*', os.getenv 'ROAM_DIR' .. '/daily/*' },
       org_default_notes_file = '~/orgfiles/refile.org',
-      vim.api.nvim_create_autocmd('FileType', {
-        pattern = 'org',
-        callback = function()
-          vim.keymap.set('i', '<M-CR>', function()
-            require('orgmode').action 'org_insert_heading_respect_content'
-          end, {
-            -- silent = true,
-            buffer = true,
-            desc = 'org_insert_heading_respect_content',
-          })
-        end,
-      }),
     }
+    
+    vim.api.nvim_create_autocmd('FileType', {
+      pattern = 'org',
+      callback = function()
+        vim.keymap.set('i', '<M-CR>', function()
+          -- Use insert_heading_respect_content which works from anywhere
+          vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes('<Esc>', true, false, true), 'n', false)
+          vim.schedule(function()
+            require('orgmode').instance().org_mappings:insert_heading_respect_content()
+          end)
+          return ''
+        end, {
+          buffer = true,
+          expr = true,
+          desc = 'Insert new heading',
+        })
+      end,
+    })
 
     -- NOTE: If you are using nvim-treesitter with ~ensure_installed = "all"~ option
     -- add ~org~ to ignore_install
